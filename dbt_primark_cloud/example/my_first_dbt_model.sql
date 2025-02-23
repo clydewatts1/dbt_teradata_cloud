f33fc9f9901130961e7310198048d7fd70c79822
Clyde Watts <Clyde.Watts267@gmail.com>
2025-02-23 20:59:19 +0000

    sss

--- dbt_primark_cloud/example/my_first_dbt_model.sql
+++ dbt_primark_cloud/example/my_first_dbt_model.sql
@@ -1,4 +1,3 @@
-
 /*
     Welcome to your first dbt model!
     Did you know that you can also configure models directly within SQL files?
@@ -11,13 +10,13 @@
 
 with source_data as (
 
-    select 1 as id
-    union all
-    select null as id
+    select 1 as id from dbc.dbcinfo
+    union distinct
+    select null as id from dbc.dbcinfo
 
 )
 
-select *
+select id
 from source_data
 
 /*
